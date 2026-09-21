2442ef8851e6e1fd75534a89d8a571136e24ed05
BorisMilev8 <stormbringers278@gmail.com>
2026-09-20 23:06:18 -0400

    Add pytest Flask fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+from app import create_app
+
+
+@pytest.fixture()
+def app():
+    app = create_app(
+        {
+            "TESTING": True,
+            "SECRET_KEY": "test-secret",
+        }
+    )
+    yield app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
